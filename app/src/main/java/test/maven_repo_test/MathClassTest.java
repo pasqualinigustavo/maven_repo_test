e593dd9ea7b39528c4878d6e1aa8b86139ce8bf7
Gustavo Luiz Pasqualini <pasqualini.gustavo@gmail.com>
2016-10-24 13:27:24 -0200

    Maven repository test

---
+++ app/src/main/java/test/maven_repo_test/MathClassTest.java
@@ -0,0 +1,15 @@
+package test.maven_repo_test;
+
+/**
+ * Created by pasqualini on 10/24/16.
+ */
+
+public class MathClassTest {
+
+
+    static float add(float number1, float number2)
+    {
+        return number1+number2;
+    }
+
+}
